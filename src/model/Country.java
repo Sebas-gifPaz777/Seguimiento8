29be2c278460f2355cdfb9091fd7d4330eee883e
Sebas-gifPaz777 <pazsebas797@gmail.com>
2022-03-18 08:55:21 -0500

    First commit

---
+++ src/model/Country.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Country {
+
+	private int goldM;
+	private int silverM;
+	private int bronzeM;
+	private int goldF;
+	private int silverF;
+	private int bronzeF;
+	private String nameC;
+	public Country(String nameC, int goldM, int silverM, int bronzeM, int goldF, int silverF, int bronzeF) {
+		this.goldM = goldM;
+		this.silverM = silverM;
+		this.bronzeM = bronzeM;
+		this.goldF = goldF;
+		this.silverF = silverF;
+		this.bronzeF = bronzeF;
+	}
+	
+	
+
+}
---
+++ src/ui/Main.java
@@ -0,0 +1,22 @@
+package ui;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static InputStreamReader isr= new InputStreamReader(System.in);
+	public static BufferedReader br= new BufferedReader(isr);
+	
+	public static void main(String[]args) {
+		
+	}
+	
+	public void input() throws IOException {
+		System.out.println("Digite el número de paises entrantes");
+		int num=br.read();
+		System.out.println("Digite la los paises con sus medallas:\n Oro hombre;Plata hombre, Bronce hombre, Oro mujer, Plata mujer, Bronce mujer");
+		S
+	}
+}
